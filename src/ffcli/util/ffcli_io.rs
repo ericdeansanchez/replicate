1ca4d540829f8bec39c5fecd61103cea4034bced
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-08-30 19:26:20 -0700

    drop s's

--- src/ffcli/util/ffcli_io.rs
+++ src/ffcli/util/ffcli_io.rs
@@ -4,14 +4,14 @@ use std::path::Path;
 
 use crate::util::errors::Result;
 
-/// Writes the contents to the given `path`.
+/// Write the contents to the given `path`.
 pub fn write<P: AsRef<Path>>(path: P, contents: &[u8]) -> Result<()> {
     let mut f = File::create(path.as_ref())?;
     f.write_all(contents)?;
     Ok(())
 }
 
-/// Writes the contents to the given `path` in "append mode".
+/// Write the contents to the given `path` in "append mode".
 pub fn append(path: &Path, contents: &[u8]) -> Result<()> {
     let mut f = OpenOptions::new()
         .write(true)
@@ -22,12 +22,12 @@ pub fn append(path: &Path, contents: &[u8]) -> Result<()> {
     Ok(())
 }
 
-/// Removes a single file specified by `path`.
+/// Remove a single file specified by `path`.
 pub fn remove_file<P: AsRef<Path>>(path: P) -> io::Result<()> {
     fs::remove_file(path.as_ref())
 }
 
-/// Reads a file from the given `path`.
+/// Read a file from the given `path`.
 pub fn read<P: AsRef<Path>>(path: P) -> io::Result<String> {
     fs::read_to_string(path)
 }
