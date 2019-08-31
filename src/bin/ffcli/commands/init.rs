dcc1fb343607f9e231e6d544ae8a88ba34c3f628
ericdeansanchez <ericdeansanchez@berkeley.edu>
2019-08-30 19:57:28 -0700

    call cargo but fail loudly if call fails

--- src/bin/ffcli/commands/init.rs
+++ src/bin/ffcli/commands/init.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::io;
-use std::process::{Command, ExitStatus, Output};
+use std::io::{self, Write};
+use std::process::{exit, Command, Output};
 
 use ffcli::command_prelude::{App, Arg, SubCommand};
 use ffcli::{FfcliError, Result};
@@ -17,8 +17,18 @@ pub fn cli() -> App {
 
 pub fn init(app: String) -> Result<()> {
     match call_cargo_new(&app) {
+        Ok(output) => {
+            if output.status.success() {
+                restructure_app(app)
+            } else {
+                io::stderr().write_fmt(format_args!(
+                    "error: call to `cargo new {}` failed, exiting...",
+                    &app
+                ))?;
+                exit(1);
+            }
+        }
         Err(e) => Err(FfcliError::Io(e)),
-        Ok(_) => restructure_app(&app),
     }
 }
 
@@ -26,10 +36,16 @@ fn call_cargo_new(app: &str) -> io::Result<Output> {
     Command::new("cargo").arg("new").arg(app).output()
 }
 
-fn restructure_app(app: &str) -> Result<()> {
+fn restructure_app(app: String) -> Result<()> {
+    create_bin()?;
+    create_lib()?;
     Ok(())
 }
 
-fn create_bin() {}
+fn create_bin() -> Result<()> {
+    Ok(())
+}
 
-fn create_lib() {}
+fn create_lib() -> Result<()> {
+    Ok(())
+}
